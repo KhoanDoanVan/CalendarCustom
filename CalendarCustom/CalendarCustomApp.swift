1da63544fe9c43cb57be8f50d965fc956ecb94bd
khoanisdev <doanvankhoan124@gmail.com>
2024-12-03 20:29:25 +0700

    Initial Commit

---
+++ CalendarCustom/CalendarCustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCustomApp.swift
+//  CalendarCustom
+//
+//  Created by Đoàn Văn Khoan on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarCustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
